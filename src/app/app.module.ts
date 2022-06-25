85817bdd46ec6f64844582abcfdcb7dddb311b2d
Emre GÖREN <emregoren98@yandex.com>
2022-06-26 01:44:40 +0200

    app-compone deleted todo-component added with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-
+import { TodoComponent } from './todo/todo.component';
+//If we want to add any component to app module, we just enough writing to CLI "ng generate component <c_name>"
 @NgModule({
   declarations: [
-    AppComponent
+    TodoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TodoComponent]
 })
 export class AppModule { }
