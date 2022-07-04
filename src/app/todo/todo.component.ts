2492478a69f2342153c66324d25aed8a6c04e1c3
Emre GOREN <emregoren98@yandex.com>
2022-07-04 02:26:59 +0200

    create class and define list

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoItem } from '../todoItem'; // When we define ToDoItem[], this import code autommatically created.
 
 @Component({
   selector: 'app-todo',
@@ -8,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
 
   constructor() { }
+
+  private name = "Emre"; //Variable declaration
+
+  public items: ToDoItem[] = [
+    new ToDoItem("kahvalti","yes"),
+    new ToDoItem("spor","yes"),
+    new ToDoItem("alisveris","no"),
+  ]; // Define list with complex type
+
+  //As we do on other language we should define a class model for list etc. For we doing this firstly let we create '.ts' file inside of the app folder.
+
+
 
   ngOnInit(): void {
   }
